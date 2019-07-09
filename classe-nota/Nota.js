6a88fedbd67cbf6d32d848d079656719c19710cb
Anderson Barros <anderson.barros0298@gmail.com>
2019-07-09 20:12:21 -0300

    comentando

--- classe-nota/Nota.js
+++ classe-nota/Nota.js
@@ -207,20 +207,21 @@ function changeNote(_note) {
     }
 }
 let idLineJoin = 0;
+//id para as linhas de juncao das notas...
 
 function createLineJoin() {
     const objLine = {},
+    //instanciando o objeto...
         fistNote = document.getElementById('nota' + (id - 2)),
         secondNote = document.getElementById('nota' + (id - 1)),
+        //pegando as notas, da esqueda para a direita...
         transformFistNote = fistNote.getAttributeNS(null, 'transform', this.localName),
         transformSecondNote = secondNote.getAttributeNS(null, 'transform', this.localName),
         aux1 = transformFistNote.split(' '),
         aux2 = transformSecondNote.split(' '),
         lineOrigin = fistNote.getAttributeNS(null, 'lineOrigin', this.localName);
     objLine.x1 = returnPositionX_porcentagemSVG(apenasNumeros(aux1[0]) + 37);
-    objLine.n1 = 'nota' + (id - 2);
     objLine.x2 = returnPositionX_porcentagemSVG(apenasNumeros(aux2[0]) + 40);
-    objLine.n2 = 'nota' + (id - 1);
     objLine.y1 = returnPositionY(secondNote.getAttributeNS(null, 'lineOrigin', this.localName)) - 13.6;
     objLine.y2 = returnPositionY(secondNote.getAttributeNS(null, 'lineOrigin', this.localName)) - 13.6;
     objLine.name = fistNote.getAttributeNS(null, 'name', this.localName);
@@ -228,10 +229,14 @@ function createLineJoin() {
     objLine.classe = 'linejoincolcheia';
     objLine.idName = 'join' + objLine.name + idLineJoin;
     objLine.idDiv = 'idSVG' + objLine.mom;
+    //setando os atributos da linha...
     new createLine(objLine);
+    //criando uma nova linha, que ira ligar as notas...
     idLineJoin++;
     fistNote.setAttributeNS(null, 'x1y1Line', objLine.idName + '-' + objLine.idDiv);
     secondNote.setAttributeNS(null, 'x2y2Line', objLine.idName + '-' + objLine.idDiv);
+    //colocando o id da linha nas notas, para saber qual a parte da linha que ira se
+    //mover juntamente com a linha...
 }
 
 function DeleteNote(tentativa = 0) {
