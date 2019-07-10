cd06f37466d9669c80663f41f11f5e1efe6ef007
Anderson Barros <anderson.barros0298@gmail.com>
2019-07-09 21:35:45 -0300

    juntar varias notas

--- classe-nota/Nota.js
+++ classe-nota/Nota.js
@@ -53,17 +53,22 @@ function PositionNote(_name, _amount = 1) {
                     createNote(_name, obj);
                     obj.x = x;
                     createNote(_name, obj);
+                    createLineJoin();
                     obj.x = x + 40;
                     createNote(_name, obj);
+                    createLineJoin();
                 } else if (_amount == 4) {
                     obj.x = x - 60;
                     createNote(_name, obj);
                     obj.x = x - 20;
                     createNote(_name, obj);
+                    createLineJoin();
                     obj.x = x + 20;
                     createNote(_name, obj);
+                    createLineJoin();
                     obj.x = x + 60;
                     createNote(_name, obj);
+                    createLineJoin();
                 }
                 for (let i = 0; i < button.length; i++) {
                     button[i].removeAttribute('disabled', 'true');
@@ -113,7 +118,7 @@ let idLineJoin = 0;
 
 function createLineJoin() {
     const objLine = {},
-    //instanciando o objeto...
+        //instanciando o objeto...
         fistNote = document.getElementById('nota' + (id - 2)),
         secondNote = document.getElementById('nota' + (id - 1)),
         //pegando as notas, da esqueda para a direita...
@@ -152,5 +157,104 @@ function DeleteNote(tentativa = 0) {
             if (!!click.match(regex)) remove_id(click);
             else DeleteNote(tentativa++);
         });
+    }
+}
+
+function join() {
+    //funcao para juntar as notas, colocar uma linha em cima delas...
+    let Notas = [];
+    //vetor para guadar as notas selecionadas...
+    let numeroDeNotas = 0;
+    //variavel numeroDeNotas para garantir que duas notas sejam selecionadas...
+    $('.svg').one('click', (e) => {
+        //pegando a primeira nota...
+        if (e.target && e.target.classList.contains('nota')) {
+            //nota encontrada...
+            if (Notas.indexOf(e.target.id) == -1) {
+                //se a nota ainda nao tiver sido selecionada...
+                Notas.push(e.target.id);
+                //nota adicionada...
+                numeroDeNotas++;
+            }
+            if (numeroDeNotas < 2) {
+                $('.svg').one('click', (e) => {
+                    //pegando a segunda nota...
+                    if (e.target && e.target.classList.contains('nota')) {
+                        //nota encontrada...
+                        if (Notas.indexOf(e.target.id) == -1) {
+                            //se a nota ainda nao tiver sido selecionada...
+                            Notas.push(e.target.id);
+                            //nota adicionada...
+                            numeroDeNotas++;
+                        }
+                        if (numeroDeNotas == 2) {
+                            let nota1 = document.getElementById(Notas[0]),
+                                trans = nota1.getAttributeNS(null, 'transform', this.localName),
+                                a = trans.split(' '),
+                                x1 = returnPositionX_porcentagemSVG(apenasNumeros(a[0])),
+                                y1 = returnPositionY(nota1.getAttributeNS(null, 'lineOrigin', this.localName)) - 13.5;
+
+                            let nota2 = document.getElementById(Notas[1]);
+                            trans = nota2.getAttributeNS(null, 'transform', this.localName);
+                            let b = trans.split(' '),
+                                x2 = returnPositionX_porcentagemSVG(apenasNumeros(b[0]));
+                            y2 = returnPositionY(nota2.getAttributeNS(null, 'lineOrigin', this.localName)) - 13.5;
+                            if (x2 < x1) {
+                                //se a segunda nota for clicada primeiro...
+                                let aux = x2;
+                                x2 = x1;
+                                x1 = aux;
+                                aux = y2;
+                                y2 = y1;
+                                y1 = aux;
+                            }
+                            let obj = {
+                                name: nota1.getAttributeNS(null, 'name', this.localName),
+                                x1: x1,
+                                y1: y1,
+                                x2: x2,
+                                y2: y2,
+                                mom: nota1.getAttributeNS(null, 'lineOrigin', this.localName)
+                            };
+                            obj.mom = apenasNumeros(obj.mom.substring(obj.mom.length - 3, obj.mom.length));
+                            if (changeNote(nota2) && changeNote(nota1)) createLineJoin(obj);
+                            //se a troca das duas notas for bem suscedida, cria a linha de uniao entre elas...
+                            else remove_id(nota2.id), remove_id(nota1.id);
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
+
+function changeNote(_note) {
+    //funcao para trocar as notas...
+    try {
+        let objSeminima = new getImagem('seminima');
+        _note.removeAttributeNS(null, 'd', this.localName);
+        _note.setAttributeNS(null, 'd', objSeminima.imagem);
+        let trans = _note.getAttributeNS(null, 'transform', this.localName),
+            a = trans.split(' '),
+            x = apenasNumeros(a[0]),
+            y = apenasNumeros(a[1]);
+        if (_note.getAttributeNS(null, 'name', this.localName) === 'colcheia') {
+            x -= 5;
+            _note.removeAttributeNS(null, 'transform', this.localName);
+            //removendo o atributo antigo...
+            _note.setAttributeNS(null, 'transform', 'translate(' + x + ' ' + y + ')');
+            //inserindo um novo atributo com a posicao atual
+            return true;
+        } else if (_note.getAttributeNS(null, 'name', this.localName) === 'semicolcheia') {
+            x = x - 5;
+            _note.removeAttributeNS(null, 'transform', this.localName);
+            //removendo o atributo antigo...
+            _note.setAttributeNS(null, 'transform', 'translate(' + x + ' ' + y + ')');
+            //inserindo um novo atributo com a posicao atual
+            return true;
+        }
+    } catch {
+        //se acontecer algum erro...
+        return false;
     }
 }
