4392bf2ef562714fe1d95a64942d7457ed84d400
asbarros <anderson.barros0298@gmail.com>
2019-07-18 21:51:14 -0300

    criando apenas uma linha entre as notas

--- classe-nota/Nota.js
+++ classe-nota/Nota.js
@@ -119,33 +119,38 @@ let idLineJoin = 0;
 //id para as linhas de juncao das notas...
 
 function createLineJoin(idNote1 = id, idNote2 = id) {
-    const objLine = {},
-        //instanciando o objeto...
-        fistNote = document.getElementById('nota' + (idNote1 - 2)),
+    const fistNote = document.getElementById('nota' + (idNote1 - 2)),
         secondNote = document.getElementById('nota' + (idNote2 - 1)),
         //pegando as notas, da esqueda para a direita...
-        transformFistNote = fistNote.getAttributeNS(null, 'transform'),
-        transformSecondNote = secondNote.getAttributeNS(null, 'transform'),
-        aux1 = transformFistNote.split(' '),
-        aux2 = transformSecondNote.split(' '),
-        lineOrigin = fistNote.getAttributeNS(null, 'lineOrigin', this.localName);
-    objLine.x1 = returnPositionX_porcentagemSVG(apenasNumeros(aux1[0]) + 37);
-    objLine.x2 = returnPositionX_porcentagemSVG(apenasNumeros(aux2[0]) + 40);
-    objLine.y1 = returnPositionY(fistNote.getAttributeNS(null, 'lineOrigin')) - 13.6;
-    objLine.y2 = returnPositionY(secondNote.getAttributeNS(null, 'lineOrigin')) - 13.6;
-    objLine.name = fistNote.getAttributeNS(null, 'name', this.localName);
-    objLine.mom = apenasNumeros(lineOrigin.substring(lineOrigin.length - 3, lineOrigin.length));
-    objLine.classe = 'linejoincolcheia';
-    objLine.idName = 'join' + objLine.name + idLineJoin;
-    objLine.idDiv = 'idSVG' + objLine.mom;
-    //setando os atributos da linha...
-    new createLine(objLine);
-    //criando uma nova linha, que ira ligar as notas...
-    idLineJoin++;
-    fistNote.setAttributeNS(null, 'x1y1Line', objLine.idName + '-' + objLine.idDiv);
-    secondNote.setAttributeNS(null, 'x2y2Line', objLine.idName + '-' + objLine.idDiv);
-    //colocando o id da linha nas notas, para saber qual a parte da linha que ira se
-    //mover juntamente com a linha...
+        line1 = fistNote.getAttributeNS(null, 'x1y1Line'),
+        line2 = secondNote.getAttributeNS(null, 'x2y2Line');
+    if (!line1 || !line2) {
+        //se existir uma linha que liga as notas, nao sera criada uma nova...
+        const objLine = {},
+            //instanciando o objeto...
+            transformFistNote = fistNote.getAttributeNS(null, 'transform'),
+            transformSecondNote = secondNote.getAttributeNS(null, 'transform'),
+            aux1 = transformFistNote.split(' '),
+            aux2 = transformSecondNote.split(' '),
+            lineOrigin = fistNote.getAttributeNS(null, 'lineOrigin', this.localName);
+        objLine.x1 = returnPositionX_porcentagemSVG(apenasNumeros(aux1[0]) + 37);
+        objLine.x2 = returnPositionX_porcentagemSVG(apenasNumeros(aux2[0]) + 40);
+        objLine.y1 = returnPositionY(fistNote.getAttributeNS(null, 'lineOrigin')) - 13.6;
+        objLine.y2 = returnPositionY(secondNote.getAttributeNS(null, 'lineOrigin')) - 13.6;
+        objLine.name = fistNote.getAttributeNS(null, 'name', this.localName);
+        objLine.mom = apenasNumeros(lineOrigin.substring(lineOrigin.length - 3, lineOrigin.length));
+        objLine.classe = 'linejoincolcheia';
+        objLine.idName = 'join' + objLine.name + idLineJoin;
+        objLine.idDiv = 'idSVG' + objLine.mom;
+        //setando os atributos da linha...
+        new createLine(objLine);
+        //criando uma nova linha, que ira ligar as notas...
+        idLineJoin++;
+        fistNote.setAttributeNS(null, 'x1y1Line', objLine.idName + '-' + objLine.idDiv);
+        secondNote.setAttributeNS(null, 'x2y2Line', objLine.idName + '-' + objLine.idDiv);
+        //colocando o id da linha nas notas, para saber qual a parte da linha que ira se
+        //mover juntamente com a linha...
+    }
 }
 
 function DeleteNote(tentativa = 0) {
